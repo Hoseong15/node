ec981527034a57ea1f192951a45e6315989517e3
Hoseong15 <dlsghtjd3982@gmail.com>
2023-08-17 22:12:54 +0900

    edit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 app.use(express.urlencoded({extended: true})) 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'))
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 
 //modoDb에 접속해서 데이터 넣기 // 
@@ -85,3 +87,10 @@ app.get('/detail/:id', function(요청,응답) {
     응답.render('detail.ejs', { data : 결과})
   })
 })
+
+app.get('/edit/:id', function(요청, 응답){
+  db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
+    console.log(결과)
+    응답.render('edit.ejs', { post : 결과 })
+  })
+});
